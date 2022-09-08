f16d2ffd31444ea846aa5723ba737e012f8b72f6
liam.docherty <liam.docherty@n-able.com>
2022-09-08 11:03:11 +0100

    Change to include file extension

--- e2e/specs/example.spec.ts
+++ e2e/specs/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 // TODO: THIS CAUSES THE ISSUE EVEN IF NOT USED
 import { DecimalPipe } from '@angular/common';
-import { title } from '../../src/constants';
+import { title } from '../../src/constants/title.js';
 
 test('example', async ({ page }) => {
   await page.goto('');
