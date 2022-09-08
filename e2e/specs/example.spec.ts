7b66587a61ed63e3b9b48d4c6df15c31f5bd2a62
liam.docherty <liam.docherty@n-able.com>
2022-09-08 10:19:52 +0100

    Add comment above decimal pipe

--- e2e/specs/example.spec.ts
+++ e2e/specs/example.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
+// TODO: THIS CAUSES THE ISSUE EVEN IF NOT USED
 import { DecimalPipe } from '@angular/common';
 import { title } from '../../src/constants';
 
 test('example', async ({ page }) => {
   await page.goto('');
-  const aaa = `${new DecimalPipe('en-gb').transform(1)}`;
-  // expect(await page.locator('#welcome').innerText()).toBe(title);
+  // const aaa = `${new DecimalPipe('en-gb').transform(1)}`;
+  expect(await page.locator('#welcome').innerText()).toBe(title);
 });
